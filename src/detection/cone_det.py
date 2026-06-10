ae5f555c992441d8e7653242c373ad64827e3308
FiremelonX8 <twitteranotherbrick@gmail.com>
2026-06-10 08:59:28 -0300

    wip: lacks geometry verification

--- src/detection/cone_det.py
+++ src/detection/cone_det.py
@@ -9,12 +9,15 @@
 # https://www.geeksforgeeks.org/python/filter-color-with-opencv/ for HSV filters/masks;
 # https://github.com/computervisionpro/yt/blob/main/hsv_masking/color-nemo.py for hints to defining th range of the HSV;
 # https://www.geeksforgeeks.org/python/clahe-histogram-eqalization-opencv/ for CLAHE in brightness equalisation;
+# https://learnopencv.com/contour-detection-using-opencv-python-c/ for contour detection;
+
 # MOD 1: Cap. 1.2.1 - morphological operations for eliminating unwanted noise and aggregating separated pieces;
 # MOD 1: Cap. 1.2.2 - for identifying the borders of the cones and, therefore their geometry;
 # MOD 1: Cap 1.3.1 - for normalizing the HSV filter;
 
 # Secondary Refereces:
 # https://revisitingmiwb.github.io/ for understanding white balancing;
+# https://github.com/PooyaNasiri/Object_Detection-OpenCV for comparing the current code to another cone detection algorithm
 
 from __future__ import annotations
 import cv2
@@ -31,6 +34,7 @@ YELLOW_HIGH = np.array([ 35, 255, 255])
 BLUE_LOW    = np.array([ 95, 120, 50])
 BLUE_HIGH   = np.array([135, 255, 255])
 
+# The orange cone is ignored for the first version of the algorithm presented here
 # Orange
 ORANGE_LOW  = np.array([  5, 150, 80])
 ORANGE_HIGH = np.array([ 18, 255, 255])
@@ -39,21 +43,8 @@ PATH_TO_IMAGES = '../../assets/images/'
 
 # The HSV filters/masks must account for both colors present in the target cone:
 # YELLOW: yellow and black; BLUE: blue and white
-def apply_color_mask(img, hsv, color):
-    # Define range for blue color in HSV
-    # It is here that the biggest change may occur
-    # Caution with this section
-    # The conditions are not ideal -> shall be changed
-    if color == 0:
-        lower = BLUE_LOW
-        upper = BLUE_HIGH
-    elif color == 1:
-        lower = YELLOW_LOW
-        upper = YELLOW_HIGH
-    elif color == 2:
-        lower = ORANGE_LOW
-        upper = ORANGE_HIGH
-
+def apply_color_mask(img, hsv, lower, upper):
+    
     # Create mask
     mask = cv2.inRange(hsv, lower, upper)
 
@@ -67,6 +58,8 @@ def apply_color_mask(img, hsv, color):
     
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
+    return result
 
 # For white balancing according to
 def percentile_stretch(img_bgr, percentile=95):
@@ -78,15 +71,60 @@ def percentile_stretch(img_bgr, percentile=95):
     return np.clip(img, 0, 255).astype(np.uint8)
 
 # For reducing noise - undesired bits extracted from applying the mask
-def apply_morphological_cleaning(img):
-    # img var must be in RGB to be converted as shown below
-    img_bw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    cv2.erode(img_bw)
-    cv2.dilate(img_bw)
+def clean_mask(mask):
+    # Kernel elíptico respeita melhor formas arredondadas
+    # TODO: Calibrate the morphing parameters
+    k3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    k5 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+    # 1. Opening: remove small noise (isolated points)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, k3)
+
+    # 2. Closure: closes small holes in the cone
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, k5)
+
+    # Dilates to get a more realistic color form
+    mask = cv2.dilate(mask, k3, iterations=1)
+
+    return mask
+
+def find_cone_contours(mask):
+    # convert the image to grayscale format
+    img_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    # apply binary thresholding
+    ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+    # visualize the binary image
+    cv2.imshow('Binary image', thresh)
+    cv2.waitKey(0)
+    cv2.imwrite('image_thres1.jpg', thresh)
+    cv2.destroyAllWindows()
+    
+    # detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+                                        
+    # draw contours on the original image
+    image_copy = mask.copy()
+    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+                    
+    # see the results
+    cv2.imshow('None approximation', image_copy)
+    cv2.waitKey(0)
+    cv2.imwrite('contours_none_image1.jpg', image_copy)
+    cv2.destroyAllWindows()
+
+# def find_cone_contours(mask):
+#     contours, _ = cv2.findContours(
+#         mask,
+#         # For external contours:
+#         cv2.RETR_EXTERNAL,
+#         cv2.CHAIN_APPROX_SIMPLE
+#     )
+#     return contours
     
 # # For recognizing the cone geometry
 # # TODO: Develop the algorithm and apply the contour filters using cv2 -> a source was not searched yet.
-# def apply_contour_detection(img):
+def filter_contours(contours, img_height):
+    pass
     
 
 # https://www.geeksforgeeks.org/python/clahe-histogram-eqalization-opencv/
@@ -142,6 +180,8 @@ def preprocess(img) -> cv2.typing.MatLike:
     return hsv
 
 def main():
+    masks = []
+    
     # Read the entirety of the image passed through its path
     # TODO: generalize the image path to make the modifictions easier
     img = cv2.imread(PATH_TO_IMAGES + '000004.png')
@@ -159,13 +199,45 @@ def main():
 
     # Prints a complete plot for viewing relevant information
     plot(img_rgb, hsv)
+    
+    mask_yellow, mask_blue, mask_orange = cv2.typing.MatLike
 
     # Loops for color in colors
     # Currently: [YELLOW, BLUE]
     colors = [0, 1]
     for color in colors:
+        if color == 0:
+            lower = BLUE_LOW
+            upper = BLUE_HIGH
+        elif color == 1:
+            lower = YELLOW_LOW
+            upper = YELLOW_HIGH
+        elif color == 2:
+            lower = ORANGE_LOW
+            upper = ORANGE_HIGH
         # Calls function to apply mask color according to the color
-        apply_color_mask(img, hsv, color)
+        mask = apply_color_mask(img, hsv, lower, upper)
+        masks.append(mask)
+        
+    # dict pointing to the contourned imgs
+    results = {
+            "yellow": [],
+            "blue": [],
+            "orange": []
+        }
+
+    for color, mask in [("blue",   masks[0]),
+                        ("yellow", masks[1])
+                        ]:
+
+        # Limpeza morfológica
+        clean = clean_mask(mask)
+
+        # Extração e filtragem de contornos
+        contours  = find_cone_contours(clean)
+        candidates = filter_contours(contours, hsv.shape[0])
+
+        results[color] = candidates
     
 
     print("Exiting...")  # Confirm exit
